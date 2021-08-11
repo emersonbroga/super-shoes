18d0d66115c7014506c9738b53f159b799a51846
Emerson Broga <emerson@emersonbroga.com>
2021-08-11 09:46:36 -0300

    Initial Readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# super-shoes
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+# Carousel com React
+
+Um projeto bem bacana pra você apresentar no seu portfolio e que demonstra que você tem conhecimentos de css e javascript é o carousel. Um projeto simples porem desafiador que geralmente é o projeto que pedem em entrevistas de emprego.
+
+Esse projeto é parte do video que foi disponibilizado no meu canal do youtube.
+
+[![!Carousel com React](http://i3.ytimg.com/vi/cX0N3TNxumw/maxresdefault.jpg)](https://www.youtube.com/watch?v=cX0N3TNxumw)
+
+Esse repositório e o video têm fins educativos.
+
+#### Se ainda não segue, veja minhas outras Redes Sociais
+
+[![instagram.com/emersonbrogadev](https://github.com/emersonbroga/social-media-snippets/blob/master/static/instagram.png?raw=true)](https://emersonbroga.com/instagram) 
+[![youtube.com/c/emersonbrogadev](https://github.com/emersonbroga/social-media-snippets/blob/master/static/youtube.png?raw=true)](https://emersonbroga.com/youtube)
+[![facebook.com/emersonbrogadev](https://github.com/emersonbroga/social-media-snippets/blob/master/static/facebook.png?raw=true)](https://emersonbroga.com/facebook)
+[![twitter.com/emersonbrogadev](https://github.com/emersonbroga/social-media-snippets/blob/master/static/twitter.png?raw=true)](https://emersonbroga.com/twitter)
+[![github.com/emersonbroga](https://github.com/emersonbroga/social-media-snippets/blob/master/static/github.png?raw=true)](https://emersonbroga.com/github)
+
+
+## Você fez também?
+
+Se você fizer o projeto tambem, me manda o link que eu coloco aqui, ou então envia uma PR!
+
+- [@emersonbroga](https://github.com/emersonbroga/super-shoes)
+
+  
+
+
+
+  
---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState, useRef } from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/static/shoes.json')
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
+  if (!data || !data.length) return null;
+
+  return (
+    <div className="container">
+      <div className="logo">
+        <img src="/static/images/super-shoes.png" alt="Super Shoes Logo" />
+      </div>
+      <div className="carousel" ref={carousel}>
+        {data.map((item) => {
+          const { id, name, price, oldPrice, image } = item;
+          return (
+            <div className="item" key={id}>
+              <div className="image">
+                <img src={image} alt={name} />
+              </div>
+              <div className="info">
+                <span className="name">{name}</span>
+                <span className="oldPrice">U$ {oldPrice}</span>
+                <span className="price">U$ {price}</span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="buttons">
+        <button onClick={handleLeftClick}>
+          <img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Left" />
+        </button>
+        <button onClick={handleRightClick}>
+          <img src="/static/images/216151_right_chevron_icon.png" alt="Scroll Right" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
